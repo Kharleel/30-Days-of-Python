7a119fb259e502f443872c8abec93fdfaf45c8a1
Kharleel <129072203+Kharleel@users.noreply.github.com>
2023-11-27 15:28:45 +0000

    import math
    def calculate_square_root(number):
        if number < 0:
            return "Cannot calculate the square root of a negative number."
        else:
            return math.sqrt(number)
    
    user_input = float(input("Enter a number to calculate its square root: "))
    result = calculate_square_root(user_input)
    
    print(f"The square root of {user_input} is: {result}")

--- sqrt_calculator.py
+++ sqrt_calculator.py
@@ -1,1 +1,14 @@
+import math
+
+def calculate_square_root(number):
+    if number < 0:
+        return "Cannot calculate the square root of a negative number."
+    else:
+        return math.sqrt(number)
+
+# Example usage
+user_input = float(input("Enter a number to calculate its square root: "))
+result = calculate_square_root(user_input)
+
+print(f"The square root of {user_input} is: {result}")
 
