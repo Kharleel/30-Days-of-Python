7bdbed61bf5931a477f6c6a1f0d0b99d6c45d1e5
Kharleel <129072203+Kharleel@users.noreply.github.com>
2023-12-04 13:36:06 +0100

    Update variables.py

--- day_002/variables.py
+++ day_002/variables.py
@@ -1,1 +1,11 @@
 #Day 2: 30 Days of python programming
+firstname = "Ibrahim"
+lastname = "Suleiman"
+fullname = "Ibrahim Suleiman"
+countryName = "Nigeria"
+cityName = "Kano"
+age = 31
+year = 2023
+is_married = yes
+is_true = true
+is_light = false
